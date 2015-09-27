8b88a3a5827359c6697a56b6843129db3cf6a8c3
Valentin Zickner <zickner@querformatik.de>
2015-09-27 17:01:24 +0200

    Improved readability of controller.js, added tasks and changed map to central germany.

--- js/controller.js
+++ js/controller.js
@@ -4,6 +4,10 @@ geoTodoController.factory('Locations', function ($resource) {
     return $resource("api/locations/:id");
 });
 
+geoTodoController.factory('Tasks', function ($resource) {
+    return $resource("api/tasks/:id");
+})
+
 geoTodoController.service('geocoder', function () {
     this.geocode = function (address, outerCallback) {
         var geocoder = new google.maps.Geocoder();
@@ -23,9 +27,9 @@ geoTodoController.service('geocoder', function () {
 });
 
 geoTodoController.controller('MapCtrl', ['$scope', 'Locations', function ($scope, Locations) {
-    $scope.map = {center: {latitude: 48.1232, longitude: 8}, zoom: 8};
+    $scope.map = {center: {latitude: 51.163333, longitude: 10.447778}, zoom: 6};
 
-    Locations.query(function(data) {
+    Locations.query(function (data) {
         $scope.locations = data;
     });
 
@@ -35,7 +39,7 @@ geoTodoController.controller('MapCtrl', ['$scope', 'Locations', function ($scope
 }]);
 
 geoTodoController.controller('ListCtrl', ['$scope', 'Locations', function ($scope, Locations) {
-    Locations.query(function(data) {
+    Locations.query(function (data) {
         $scope.locations = data;
     });
 }]);
@@ -44,23 +48,25 @@ geoTodoController.controller('DetailCtrl', ['$scope', 'Locations', '$routeParams
     $scope.locationId = $routeParams.locationId;
 }]);
 
-geoTodoController.controller('NewCtrl', ['$scope', 'Locations', 'geocoder', function ($scope, Locations, geocoder) {
-    $scope.addNew = function () {
-        var result = geocoder.geocode($scope.inputLocation, function (results) {
-            Locations.save({
-                'id': Locations.length,
-                'name': $scope.inputName,
-                'location': $scope.inputLocation,
-                'latitude': results.results[0].geometry.location.G,
-                'longitude': results.results[0].geometry.location.K
-            });
-
-            $scope.inputName = "";
-            $scope.inputLocation = "";
-
-            console.log(results); //{success: true, err: undefined, results: {...} or {success:false, err: Error object, results: undefined}
+geoTodoController.controller('NewCtrl', ['$scope', 'Locations', 'Tasks', 'geocoder',
+    function ($scope, Locations, Tasks, geocoder) {
+        Tasks.query(function (data) {
+           $scope.tasks = data;
         });
 
+        $scope.addNew = function () {
+            var result = geocoder.geocode($scope.inputLocation, function (results) {
+                Locations.save({
+                    'name': $scope.inputName,
+                    'location': $scope.inputLocation,
+                    'latitude': results.results[0].geometry.location.G,
+                    'longitude': results.results[0].geometry.location.K
+                });
 
-    }
-}]);
+                $scope.inputName = "";
+                $scope.inputLocation = "";
+
+                console.log(results); //{success: true, err: undefined, results: {...} or {success:false, err: Error object, results: undefined}
+            });
+        }
+    }]);
