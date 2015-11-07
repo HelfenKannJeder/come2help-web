befa58ce3a3afc5d80c4408ce026b95bdf378da2
Joshua Gleitze <git@joshuagleitze.de>
2015-11-07 01:35:31 +0100

    Set anti caching headers on the webserver. Removed python webserver.

--- tasks/server.js
+++ tasks/server.js
@@ -2,18 +2,30 @@
  * Very simple webserver for development purposes.
  */
 
- var connect = require('connect');
- var serveStatic = require('serve-static');
- var path = require('path');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var path = require('path');
 
- var baseDir = path.resolve(__dirname, '..');
+var baseDir = path.resolve(__dirname, '..');
+
+function getWebserver() {
+	var setHeaders = function(res) {
+		res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+		res.setHeader('Pragma', 'no-cache');
+		res.setHeader('Expires', '0');
+	};
+	return serveStatic(baseDir, {
+		setHeaders: setHeaders,
+		etag: false
+	});
+}
 
 /**
  * A simple web server that will default to deliver the base directory of this repository. It can be extended through #use.
  */
- function Server() {
+function Server() {
 
-	 var server = connect();
+	var server = connect();
 
 	/**
 	 * Use the provided middleware. The order of the calls defines the order in which middlewares will be queried.
@@ -21,18 +33,18 @@
 	 * @param {Object} server A node js server middleware.
 	 * @return {void}
 	 */
-	 this.use = server.use.bind(server);
+	this.use = server.use.bind(server);
 
-	 /**
-	  * Launch the server and listen on the provided port.
-	  *
-	  * @param {number} port The port to listen on.
-	  * @return {void}
-	  */
-	 this.listen = function(port) {
-		 server.use(serveStatic(baseDir));
-		 server.listen(port);
-	 };
- }
+	/**
+	 * Launch the server and listen on the provided port.
+	 *
+	 * @param {number} port The port to listen on.
+	 * @return {void}
+	 */
+	this.listen = function(port) {
+		server.use(getWebserver());
+		server.listen(port);
+	};
+}
 
- module.exports = Server;
+module.exports = Server;
