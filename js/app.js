503e109daa49aa7e087428f019a627e2d54c98a6
Valentin Zickner <zickner@querformatik.de>
2015-11-19 22:02:09 +0100

    Removed modules which are not necessary for phase 1.

--- js/app.js
+++ js/app.js
@@ -7,30 +7,12 @@ angular.module('Come2HelpApp', [
 	.config(['$routeProvider',
 		function ($routeProvider) {
 			$routeProvider.
-				when('/map', {
-					templateUrl: 'partials/map.html',
-					controller: 'MapController',
-					controllerAs: 'ctrl'
-
-				}).
 				when('/list', {
 					templateUrl: 'partials/list.html',
 					controller: 'ListController',
 					controllerAs: 'ctrl'
 				}).
-				when('/detail/:locationId', {
-					templateUrl: 'partials/detail.html',
-					controller: 'DetailController',
-					controllerAs: 'ctrl'
-
-				}).
-				when('/new', {
-					templateUrl: 'partials/new.html',
-					controller: 'NewController',
-					controllerAs: 'ctrl'
-
-				}).
 				otherwise({
-					redirectTo: '/map'
+					redirectTo: '/list'
 				});
 		}]);
--- js/controller.js
+++ js/controller.js
@@ -4,10 +4,6 @@ angular.module('Come2HelpController').factory('Locations', ['$resource', functio
 	return $resource('api/locations/:id');
 }]);
 
-angular.module('Come2HelpController').factory('Tasks', ['$resource', function ($resource) {
-	return $resource('api/tasks/:id');
-}]);
-
 angular.module('Come2HelpController').service('geocoder', [function () {
 	this.geocode = function (address, outerCallback) {
 		var geocoder = new google.maps.Geocoder();
@@ -26,52 +22,10 @@ angular.module('Come2HelpController').service('geocoder', [function () {
 	};
 }]);
 
-angular.module('Come2HelpController').controller('MapController', ['Locations', function (Locations) {
-	var vm = this;
-
-	vm.map = {center: {latitude: 51.163333, longitude: 10.447778}, zoom: 6};
-
-	Locations.query(function (data) {
-		vm.locations = data;
-	});
-
-	vm.openDetails = function (id) {
-		location.href = '#/detail/' + id;
-	};
-}]);
-
 angular.module('Come2HelpController').controller('ListController', ['Locations', function (Locations) {
 	var vm = this;
 
 	Locations.query(function (data) {
 		vm.locations = data;
 	});
-}]);
-
-angular.module('Come2HelpController').controller('DetailController', ['Locations', '$routeParams', function (Locations, $routeParams) {
-	var vm = this;
-
-	vm.locationId = $routeParams.locationId;
-}]);
-
-angular.module('Come2HelpController').controller('NewController', ['Locations', 'Tasks', 'geocoder', function (Locations, Tasks, geocoder) {
-	var vm = this;
-
-	Tasks.query(function (data) {
-		vm.tasks = data;
-	});
-
-	vm.addNew = function () {
-		var result = geocoder.geocode(vm.inputLocation, function (results) {
-			Locations.save({
-				'name': vm.inputName,
-				'location': vm.inputLocation,
-				'latitude': results.results[0].geometry.location.G,
-				'longitude': results.results[0].geometry.location.K
-			});
-
-			vm.inputName = '';
-			vm.inputLocation = '';
-		});
-	};
 }]);
