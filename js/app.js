7c131e839b2c9d529e358558ff5aa8993926f5bd
Joshua Gleitze <git@joshuagleitze.de>
2015-11-27 22:47:16 +0100

    split config blocks in app.js to separate concerns.

--- js/app.js
+++ js/app.js
@@ -5,57 +5,57 @@ angular.module('Come2HelpApp', [
 	'pascalprecht.translate',
 	'satellizer'
 ])
-	.config(['$routeProvider', '$authProvider',
-		function ($routeProvider, $authProvider) {
-			$routeProvider.
-				when('/register', {
-					templateUrl: 'partials/register.html',
-					controller: 'RegisterController',
-					controllerAs: 'ctrl',
-					resolve: {
-						skipIfLoggedIn: skipIfLoggedIn
-					}
-				}).
-				when('/register/done', {
-					templateUrl: 'partials/registerDone.html'
-				}).
-				when('/login', {
-					templateUrl: 'partials/login.html',
-					controller: 'LoginController',
-					controllerAs: 'ctrl',
-					resolve: {
-						skipIfLoggedIn: skipIfLoggedIn
-					}
-				}).
-				otherwise({
-					redirectTo: '/register'
-				});
+.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+	when('/register', {
+		templateUrl: 'partials/register.html',
+		controller: 'RegisterController',
+		controllerAs: 'ctrl',
+		resolve: {
+			skipIfLoggedIn: skipIfLoggedIn
+		}
+	}).
+	when('/register/done', {
+		templateUrl: 'partials/registerDone.html'
+	}).
+	when('/login', {
+		templateUrl: 'partials/login.html',
+		controller: 'LoginController',
+		controllerAs: 'user',
+		resolve: {
+			skipIfLoggedIn: skipIfLoggedIn
+		}
+	}).
+	otherwise({
+		redirectTo: '/register'
+	});
 
-			$authProvider.facebook({
-				clientId: 'Facebook App ID'
-			});
+	function skipIfLoggedIn($q, $auth) {
+		var deferred = $q.defer();
+		if ($auth.isAuthenticated()) {
+			deferred.reject();
+		} else {
+			deferred.resolve();
+		}
+		return deferred.promise;
+	}
 
-			$authProvider.google({
-				clientId: 'Google Client ID'
-			});
+	function loginRequired($q, $location, $auth) {
+		var deferred = $q.defer();
+		if ($auth.isAuthenticated()) {
+			deferred.resolve();
+		} else {
+			$location.path('/login');
+		}
+		return deferred.promise;
+	}
+}])
+.config(['$authProvider', function($authProvider) {
+	$authProvider.facebook({
+		clientId: 'Facebook App ID'
+	});
 
-			function skipIfLoggedIn($q, $auth) {
-				var deferred = $q.defer();
-				if ($auth.isAuthenticated()) {
-					deferred.reject();
-				} else {
-					deferred.resolve();
-				}
-				return deferred.promise;
-			}
-
-			function loginRequired($q, $location, $auth) {
-				var deferred = $q.defer();
-				if ($auth.isAuthenticated()) {
-					deferred.resolve();
-				} else {
-					$location.path('/login');
-				}
-				return deferred.promise;
-			}
-		}]);
+	$authProvider.google({
+		clientId: 'Google Client ID'
+	});
+}]);
