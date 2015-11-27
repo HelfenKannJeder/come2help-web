343332d39ce71aaf4d5c0ebc7b3acf3d8122a741
Sebastian Richter <sebastian.max.richter@gmail.com>
2015-11-27 13:44:57 +0100

    Remove debugging output.

--- js/login.js
+++ js/login.js
@@ -2,7 +2,6 @@ angular.module('Come2HelpController')
 	.controller('LoginCtrl', ['$scope', '$location', '$auth', function($scope, $location, $auth) {
 		$scope.error = null;
 		$scope.login = function() {
-			console.log('login in LoginCtrl');
 			$auth.login($scope.user)
 				.then(function() {
 					$location.path('/');
